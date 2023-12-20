196155e2e22d76647b8d0793d46921070083c2fa
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-12-20 03:22:51 -0700

    this demonstrates how to do a relationship, use foreign keys, and serialization excluding rules

--- server/models.py
+++ server/models.py
@@ -11,22 +11,57 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    reviews = db.relationship("Review", back_populates="customer");
+    
+    #varname = association_proxy('relattribute', 'classnamelowercase',
+    #                             creator=lambda classnamelowercase_obj: ClassNameTransition(relattributeintransclass=classnamelowercase_obj))
+    
+    items = association_proxy('reviews', 'item', creator=lambda item_obj: Review(item=item_obj))
+
+    serialize_rules = ('-reviews.customer',);
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship("Review", back_populates="item");
+
+    #varname = association_proxy('relattribute', 'classnamelowercase',
+    #                             creator=lambda classnamelowercase_obj: ClassNameTransition(relattributeintransclass=classnamelowercase_obj))
+    customers = association_proxy('reviews', 'customer',
+                                 creator=lambda customer_obj: Review(customer=customer_obj));
+    
+    serialize_rules = ('-reviews.item',);
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews";
+
+    id = db.Column(db.Integer, primary_key=True);
+    comment = db.Column(db.String);
+    
+    #db.Column('colname', DataType, db.ForeignKey('dbtablename.colname'));#example only
+    #colname = db.Column(DataType, db.ForeignKey('dbtablename.colname'));#example only
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'));
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'));
+    
+    #varname = db.relationship("ClassName", back_populates="correspondingattribute");#example only
+    customer = db.relationship("Customer", back_populates="reviews");
+    item = db.relationship("Item", back_populates="reviews");
+
+    serialize_rules = ('-item.reviews', '-customer.reviews');
